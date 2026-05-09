d919e369decdc18d4c6f08af197617030b24ddc3
Kapil132005 <khadasekapil13@gmail.com>
2026-05-09 12:14:04 +0530

    updated teacher.php
    remove the script and add just script path

--- teacher/teacher.php
+++ teacher/teacher.php
@@ -198,34 +198,8 @@ session_start();
 
 </div>
 
-<!-- JS -->
+<script src="../js/script.js"></script>
 <script>
-
-const loginBtn = document.getElementById("loginBtn");
-const signupBtn = document.getElementById("signupBtn");
-
-const loginForm = document.getElementById("loginForm");
-const signupForm = document.getElementById("signupForm");
-
-loginBtn.addEventListener("click", () => {
-
-    loginBtn.classList.add("active");
-    signupBtn.classList.remove("active");
-
-    loginForm.classList.add("active");
-    signupForm.classList.remove("active");
-
-});
-
-signupBtn.addEventListener("click", () => {
-
-    signupBtn.classList.add("active");
-    loginBtn.classList.remove("active");
-
-    signupForm.classList.add("active");
-    loginForm.classList.remove("active");
-
-});
 
 function goBack()
 {
