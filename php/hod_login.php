f90ca754f09ec5aee4fbcf7db0492a6cce2e3862
Kapil132005 <khadasekapil13@gmail.com>
2026-05-09 12:28:24 +0530

    updated hod_login.php
    hash password now working with new updated code

--- php/hod_login.php
+++ php/hod_login.php
@@ -4,32 +4,47 @@ session_start();
 
 include "config.php";
 
-$email = trim($_POST['email']);
-$password = trim($_POST['password']);
+$email = $_POST['email'];
 
-$query = "SELECT * FROM hod_users WHERE email='$email'";
+$password = $_POST['password'];
 
-$result = mysqli_query($conn, $query);
+$query =
+"SELECT * FROM hod_users
+ WHERE email='$email'";
 
-if(mysqli_num_rows($result) > 0)
+$result =
+mysqli_query($conn, $query);
+
+if(mysqli_num_rows($result) == 1)
 {
-    $hod = mysqli_fetch_assoc($result);
+    $hod =
+    mysqli_fetch_assoc($result);
 
-    if($password == "admin123")
+    if(password_verify(
+        $password,
+        $hod['password']
+    ))
     {
-        $_SESSION['hod_id'] = $hod['id'];
+        $_SESSION['hod_id']
+        = $hod['id'];
 
-        $_SESSION['hod_name'] = $hod['name'];
+        $_SESSION['hod_name']
+        = $hod['name'];
 
-        header("Location: ../hod/hod_dashboard.php");
+        header(
+        "Location: ../hod/hod_dashboard.php"
+        );
 
         exit();
     }
 }
 
-$_SESSION['error'] = "Invalid Email or Password";
+$_SESSION['error'] =
+"Invalid Email or Password";
 
-header("Location: ../hod/hod.php");
+header(
+"Location: ../hod/hod.php"
+);
 
 exit();
 
